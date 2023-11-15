cb07893e573d5b4f4e356a8d523bf775dd485ff6
R0294410 <96431610+R0294410@users.noreply.github.com>
2023-11-15 19:02:58 +0000

    Deploying to gh-pages from  @ dcb0fe8caece92313836f6fa03befb7946471985 🚀

---
+++ Dobbelen/PlayerComponent.razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorApp_R0294410.Pages.Dobbelen
+{
+    public partial class PlayerComponent
+    {
+        [Parameter]
+        public string Name { get; set; }
+        [Parameter]
+        public string Color { get; set; }
+    }
+}
---
+++ Movies/Movies.razor.cs
@@ -0,0 +1,39 @@
+﻿using BlazorApp_R0294410.Models;
+using System.Text.Json;
+
+namespace BlazorApp_R0294410.Pages.Movies
+{
+    public partial class Movies
+    {
+        public string SearchTerm { get; set; }
+        public Movie[] ListMovies { get; set; }
+        public Movie NewMovie { get; set; }
+        string apiKey = "9aa2196e";
+        HttpRequestMessage request;
+        HttpResponseMessage response;
+        HttpClient client;
+        MovieResponse Response { get; set; }
+
+        public async Task SearchMovie()
+        {
+            client = new HttpClient();
+            // Send Request to API
+            request = new HttpRequestMessage()
+            {
+                // Set HTTP method
+                Method = HttpMethod.Get,
+                // Set destination of request
+                RequestUri = new Uri(@$"https://www.omdbapi.com/?s={SearchTerm}&type=movie&apikey={apiKey}")
+            };
+            // Send request and receive response
+            response = await client.SendAsync(request);
+            using Stream stream = await response.Content.ReadAsStreamAsync();
+            //stream.Flush();
+            Response = await JsonSerializer.DeserializeAsync<MovieResponse>(stream) ;
+
+            ListMovies = Response.Search;
+            StateHasChanged();
+
+        }
+    }
+}
